1eb1134c9feeaf4033b12f902c105f33c384509f
gacub20ru <gacub20ru@gmail.com>
2025-10-19 19:25:37 +0000

    Update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { objectToAuthDataMap, AuthDataValidator } from '@telegram-auth/server';
 
 @Controller()
 export class AppController {
@@ -17,16 +16,5 @@ export class AppController {
     return {
       message: 'pong',
     };
-  }
-
-  @Post('auth/telegram')
-  async authTelegram(@Body() req: any) {
-    console.log(req);
-    const botToken = process.env.BOT_TOKEN ?? '';
-    const validator = new AuthDataValidator({ botToken });
-    const data = objectToAuthDataMap(req);
-    const user = await validator.validate(data);
-
-    return user;
   }
 }
