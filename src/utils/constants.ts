7c75ec8cad7650532519272901de760c2101267d
gacub20ru <gacub20ru@gmail.com>
2025-10-22 18:20:32 +0000

    Fix variable name

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const BOT_TOKEN = process.env.BOT_TOKEN ?? ''
 export const DEV_BOT_TOKEN = process.env.DEV_BOT_TOKEN ?? ''
 export const JWT_SECRET = process.env.JWT_SECRET ?? 'secret-key'
-export const ORIGINS = (process.env.DOMAINS ?? '').split(',')
+export const ORIGINS = (process.env.ORIGINS ?? '').split(',')
