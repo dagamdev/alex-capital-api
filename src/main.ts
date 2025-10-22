3cbff9a2d604245ef4c768837cb5564b83cb0834
gacub20ru <gacub20ru@gmail.com>
2025-10-22 18:09:44 +0000

    Add orings in env variable

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import cookieParser from 'cookie-parser'
+import { ORIGINS } from './utils/constants'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: [
-      'https://lan-unmenacing-diddly.ngrok-free.dev',
-      'https://alex-capital-web.vercel.app',
-    ],
+    origin: ORIGINS,
     credentials: true,
   })
   app.use(cookieParser())
